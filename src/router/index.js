e0705edbff0f761ead0ddf8b8eae21da81d2a03b
Karem Mohamed <karemkero21@gmail.com>
2022-03-29 14:41:25 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { FavoritesContextProvider } from "../store/favorites-context";
 
 import AllMeetupsPage from "../pages/AllMeetupsPage";
